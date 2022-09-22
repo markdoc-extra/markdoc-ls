c180ba1ce5132207172945aef20181eaeef90f0e
Mohit Singh <mohit@mohitsingh.in>
2022-09-22 19:14:52 +0530

    fix(config): incorrect config cleanup

--- src/config.ts
+++ src/config.ts
@@ -8,15 +8,23 @@ const DEFAULT_DIR = 'markdoc'
 const TEMP_DIR = join(tmpdir(), 'markdoc-ls')
 
 async function buildFile(cfgRoot: string, name: string) {
+  const nameWithExt = `${name}.js`
+  const filePath = join(cfgRoot, nameWithExt)
+  const tempFilePath = join(TEMP_DIR, nameWithExt)
+
+  // create temp directory if it doesn't exists
   if (!existsSync(TEMP_DIR)) {
     mkdirSync(TEMP_DIR)
   }
-  const filePath = join(cfgRoot, `${name}.js`)
+  // remove old generated config if exists
+  if (existsSync(tempFilePath)) {
+    rmSync(tempFilePath)
+  }
+  // return empty for non-existent configs
   if (!existsSync(filePath)) {
     return {}
-  } else {
-    rmSync(filePath)
   }
+
   await build({
     entryPoints: [join(cfgRoot, name)],
     bundle: true,
